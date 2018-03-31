df3e35f700d231e19e12a9b05820cace523d22cc
Cobeaquen <37908218+Cobeaquen@users.noreply.github.com>
2018-03-31 15:12:54 +0200

    update: Parralax

--- parralax.js
+++ parralax.js
@@ -10,9 +10,12 @@ function scrolling() {
     document.getElementById("status").innerHTML = "scrolling!!";
     var currentScroll = document.body.scrollTop;
     document.getElementById("status").innerHTML = currentScroll;
-    document.getElementById("jump").style.top = currentScroll * 0.8;
-    document.getElementById("jump02").style.top = currentScroll * 0.7;
-    document.getElementById("jump03").style.top = currentScroll * 0.6;
+    document.getElementById("par01").style.top = currentScroll*.7;
+    document.getElementById("par02").style.top = currentScroll*.8;
+    document.getElementById("par03").style.top = currentScroll*.9;
+    //document.getElementById("btn").style.top = 100 + "%";
+    //document.getElementById("jump02").style.top = currentScroll * 0.7;
+    //document.getElementById("jump03").style.top = currentScroll * 0.6;
     //for (var i = 0; i < imgs.length; i++) {
     //    imgs[i].style.position = "absolute";
     //    imgs[i].style.top = (currentScroll*(i/3.5));
