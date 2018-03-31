1af78534d400c916b39ce6a7ce0db3c1a9aaf1c5
Cobeaquen <37908218+Cobeaquen@users.noreply.github.com>
2018-03-31 16:36:05 +0200

    Optimization

--- parralax.js
+++ parralax.js
@@ -1,8 +1,8 @@
-var imgs = [
-document.getElementById("par01"),
-document.getElementById("par02"),
-document.getElementById("par03"),
-];
+
+var par01 = document.getElementById("par01");
+var par02 = document.getElementById("par02");
+var par03 = document.getElementById("par03");
+
 
 addEventListener("scroll", scrolling);
 
@@ -10,14 +10,7 @@ function scrolling() {
     document.getElementById("status").innerHTML = "scrolling!!";
     var currentScroll = document.body.scrollTop;
     document.getElementById("status").innerHTML = currentScroll;
-    document.getElementById("par01").style.top = currentScroll*.7;
-    document.getElementById("par02").style.top = currentScroll*.8;
-    document.getElementById("par03").style.top = currentScroll*.9;
-    //document.getElementById("btn").style.top = 100 + "%";
-    //document.getElementById("jump02").style.top = currentScroll * 0.7;
-    //document.getElementById("jump03").style.top = currentScroll * 0.6;
-    //for (var i = 0; i < imgs.length; i++) {
-    //    imgs[i].style.position = "absolute";
-    //    imgs[i].style.top = (currentScroll*(i/3.5));
-    //}
+    par01.style.top = currentScroll*.7;
+    par02.style.top = currentScroll*.8;
+    par03.style.top = currentScroll*.9;
 }
