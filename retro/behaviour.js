446159029d3e7629a0e7f63f6740f0829a2f66af
Cobeaquen <37908218+Cobeaquen@users.noreply.github.com>
2018-05-06 02:41:08 +0200

    Scrolling

--- retro/behaviour.js
+++ retro/behaviour.js
@@ -3,11 +3,12 @@ var txt = document.getElementById("hd1");
 var bg01 = document.getElementById("bg01");
 var bg02 = document.getElementById("bg02");
 
-addEventListener("scroll", onScroll)
+//addEventListener("scroll", onScroll)
 
 function onScroll()
 {
-    var scroll = document.body.scrollTop;
+    var scroll = document.documentElement.scrollTop;
+    txt.innerHTML = scroll;
     bg01.style.top = scroll * 0.3 + px;
     bg02.style.top = scroll * 0.15 + px;
 }
