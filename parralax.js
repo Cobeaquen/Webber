61bb442fad3e78b3a135c954966e64bcd6322455
Cobeaquen <37908218+Cobeaquen@users.noreply.github.com>
2018-05-29 21:32:49 +0200

    dd

--- parralax.js
+++ parralax.js
@@ -1,12 +1,14 @@
+var px = "px";
 var par01 = document.getElementById("par01");
 var par02 = document.getElementById("par02");
 var par03 = document.getElementById("par03");
 
-addEventListener("scroll", scrolling);
-
-function scrolling() {
-    var currentScroll = document.body.scrollTop;
-    par01.style.top = currentScroll*.6 + 'px';
-    par02.style.top = currentScroll*.7 + 'px';
-    par03.style.top = currentScroll*.8 + 'px';
+function onScroll()
+{
+    var scroll = document.documentElement.scrollTop;
+    if (scroll == 0)
+        scroll = document.body.scrollTop;
+    par03.style.top = scroll * 0.425 + px;
+    par02.style.top = scroll * 0.15 + px;
+    par01.style.top = scroll * 0.05 + px;
 }
--- retro/behaviour.js
+++ retro/behaviour.js
@@ -3,15 +3,11 @@ var txt = document.getElementById("hd1");
 var bg01 = document.getElementById("bg01");
 var bg02 = document.getElementById("bg02");
 
-//addEventListener("scroll", onScroll)
-var offsetY = 100/document.body.width + px;
-bg02.style.backgroundPositionY = bg02.style.backgroundPositionY / document.body.width + "px";
-
 function onScroll()
 {
     var scroll = document.documentElement.scrollTop;
     if (scroll == 0)
         scroll = document.body.scrollTop;
     bg01.style.top = scroll * 0.425 + px;
-    bg02.style.top = "2.5%" + scroll * 0.15 + px;
+    bg02.style.top = scroll * 0.15 + px;
 }
